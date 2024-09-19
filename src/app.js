9f52538266f75f397271ad1df5a6abf15e65792d
Asgharkhattak05 <asgharkhattak05@gmail.com>
2024-09-19 11:40:11 +0500

    setup  some filesdeep in routes regex

--- src/app.js
+++ src/app.js
@@ -1,14 +1,47 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 
 
-app.use("/post" , (req, res )=>{
-  res.send("got all post")
-})
+// app.use("/", (req, res) => {
+//   res.send("server is listning successfully");
+// });
+// app.use("/post", (req, res)=>{
+//   res.send("got the post data from db")
+// })
+// app.use("/post", (req, res)=>{
+//   console.log(req.body)
+//   res.send("data post successfully to db")
+// })
 
-app.use("/" , (req, res)=>{
-    res.send("server is listning successfully")
-})
-app.listen(3000)
+// app.get("/post", (req, res)=>{
+//     res.send("got the post data from db")
+//   })
+
+
+  // app.get("/ab?c", (req, res)=>{
+  //   res.send("got the  data from db")
+  // })
+  // app.get("/ab*cd", (req, res)=>{
+  //   res.send("got the  data from db")
+  // })
+  // app.get("/ab+c", (req, res)=>{
+  //   res.send("got the  data from db")
+  // })
+
+
+  // app.get("/a/", (req, res)=>{
+  //   res.send("got the  data from db")
+  // })
+
+
+  app.get("/*a$/", (req, res)=>{
+    res.send("got the  data from db")
+  })
+
+  // app.post("/post", (req, res)=>{
+  //   console.log(req.params)
+  //   res.send("data post successfully to db")
+  // })
+app.listen(3000);
