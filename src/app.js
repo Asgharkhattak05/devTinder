8d822b697da77b90567f090c1496a4b157978134
Asgharkhattak05 <asgharkhattak05@gmail.com>
2024-10-07 22:47:21 +0500

    schema validation added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const User = require("./models/user.model.js");
 require("./config/database.js");
 
 const app = express();
+
+app.use(express.json());
+
 connectDB()
   .then(() => {
     console.log("MongoDB connected successfully...");
@@ -15,29 +18,75 @@ connectDB()
     console.error(err, "Error connecting to MongoDB");
   });
 
-app.post("/signup", async (req, res) => {
-  const user = {
-    firstName: "John",
-    lastName: "Doe",
-    email: "johndo@example.com",
-    password: "password123",
-    age: 26,
-    gender: "Male",
-  };
-
-  // create instance of User model to save to database
-  const newUser = new User(user);
-
+app.get("/feed", async (req, res) => {
   try {
-    await newUser.save();
-    res
-      .status(200)
-      .send({ message: "User saved successfully!", user: newUser });
+    const users = await User.find({});
+    res.status(200).send({ message: "Feed fetched successfully", users });
   } catch (error) {
-    console.error(error);
-    res.status(400).send("Unable to save the user to the database");
+    res.status(500).send("Error fetching the feed");
   }
 });
+
+app.post("/signup", async (req, res) => {
+  // create instance of User model to save to database
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(200).send({ message: "User saved successfully!", user });
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Unable to save the user to the database" + error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body?.email;
+
+  try {
+    const user = await User.findOne({ email: userEmail });
+    if (!user) return res.status(404).send({ message: "User not found" });
+    else res.status(200).send({ message: "User get successfully", user });
+  } catch (error) {
+    res.status(404).send({ message: "error while fetching user", error });
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // await User.findByIdAndDelete(userId);
+    await User.findByIdAndDelete({ _id: userId });
+    res.status(200).send({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(404).send({ message: "error while deleteing user", error });
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const updatedFields = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      updatedFields,
+      { 
+        new: true,              // return the updated document
+        runValidators: true      // validate before updating
+      }
+    );
+    
+    if (!updatedUser) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    
+    res.status(200).send({ message: "User updated successfully", updatedUser });
+  } catch (error) {
+    res.status(400).send({ message: "Error while updating user", error });
+  }
+});
+
 
 app.use("/", (req, res) => {
   res.send("Welcome to the DevTinder API!");
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,28 +5,46 @@ const userSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
+      minLength: 4,
+      maxLength: 40,
     },
     lastName: {
       type: String,
-      required: true,
+      minLength: 4,
+      maxLength: 40,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
       type: String,
       required: true,
+      minLength: 8,
+      maxLength: 50,
     },
     age: {
       type: Number,
-      required: true,
+      min: 18,
     },
     gender: {
       type: String,
-      required: true,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender must be either male, female or other");
+        }
+      },
     },
+    image:{
+      type:String,
+      default:"https://thumbs.dreamstime.com/b/vector-illustration-avatar-dummy-logo-set-avatar-image-vector-icon-stock-vector-design-avatar-dummy-sign-137158788.jpg"
+    },
+    skills:{
+      type:[String]
+    }
   },
   { timestamps: true }
 );
