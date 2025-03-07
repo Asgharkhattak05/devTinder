cf154d7f42da446fc3d4cc7997916ab0154777c3
Asghar Waheed <asgharkhattak789@gmail.com>
2025-03-07 21:32:31 +0500

    advanced routes

--- src/app.js
+++ src/app.js
@@ -2,15 +2,39 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send("user get successfully");
-});
-app.post("/user", (req, res) => {
-  res.send("user data post successfully");
-});
+// app.get("/ab?c", (req, res) => {
+//   res.send("user get successfully");
+// });
+// app.get("/ab+c", (req, res) => {
+//   res.send("user get successfully");
+// });
+// app.get("/a*c", (req, res) => {
+//   res.send("user get successfully");
+// });
+// app.get("/a(bc)+d", (req, res) => {
+//   res.send("user get successfully");
+// });
+// app.get("/a/", (req, res) => {
+//   res.send("user get successfully");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("hello got you on the test");
+// app.get("/user/:id", (req, res) => {
+//   console.log(req.params);
+//   res.send("user get successfully");
+// });
+
+// params is optional here
+
+// app.get("/user/:id?", (req, res) => {
+//   console.log(req.params);
+//   res.send("user get successfully");
+// });
+
+// for query parameters
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send("user get successfully");
 });
 
 app.listen(3000, () => {
