20cf6f7a2b4e5050ecee5abeeea1eecba5cee69a
Asghar Waheed <asgharkhattak789@gmail.com>
2025-03-08 22:59:59 +0500

    user routes crud

--- src/app.js
+++ src/app.js
@@ -1,10 +1,77 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
+const { User } = require("./models/user.model.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  // console.log(req.body);
+
+  // Creating a new instance of the User model (a new user document)
+  const user = new User(req.body);
+  try {
+    await user.save(); // Saving the user document to the MongoDB database
+    res.status(201).send("User Created Successfully");
+  } catch (error) {
+    res.status(400).send("Error While Creating User: " + error.message);
+  }
+});
+
+// get user by email
+app.get("/user", async (req, res) => {
+  const email = req.body.email;
+  console.log(email);
+  try {
+    const users = await User.find({ email: email });
+    console.log(users);
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (error) {
+    res.status(400).send("erroe while getting user by id");
+  }
+});
+//  feed api
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("users not found");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (error) {
+    res.status(400).send("erroe while getting all user");
+  }
+});
+//  feed api
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.userId);
+    res.status(200).send("user deleted successfully");
+  } catch (error) {
+    res.status(400).send("erroe while deleting a user");
+  }
+});
+//  update user api
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const _id = req.body._id;
+  try {
+    await User.findByIdAndUpdate({ _id }, data);
+    res.status(200).send("user Updated successfully");
+  } catch (error) {
+    res.status(400).send("erroe while updating a user");
+  }
+});
+
 connectDB()
   .then(() => {
+    // Starting the server only after a successful database connection
     app.listen(PORT, () => {
       console.log(`🚀 Server is running on port ${PORT}`);
     });
@@ -16,6 +83,7 @@ connectDB()
     );
   });
 
+// Handling graceful shutdown when the server is stopped (Ctrl+C)
 process.on("SIGINT", async () => {
   console.log("🛑 Shutting down server...");
   await mongoose.connection.close();
---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+// This creates a Mongoose model for the "users" collection.
+// To create a new user document in the database, we must first create an instance of this model and then save it.
+
+module.exports = { User };
